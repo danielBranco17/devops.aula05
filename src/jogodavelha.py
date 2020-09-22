65bc70105762a82f6e4518e91105bace5d9e35c6
Daniel <do29137@gmail.com>
2020-09-21 21:37:25 -0300

    AC2

--- src/jogodavelha.py
+++ src/jogodavelha.py
@@ -1,14 +1,14 @@
 def inicializar():
-	tab = [ ]
+	tab = []
 	for i in range(3):
-		linha = [ ]
+		linha = []
 		for j in range(3):
 			linha.append(".")
 		tab.append(linha)
 	return tab
 
-def main( ):
-	jogo = inicializar( )
+def main():
+	jogo = inicializar()
 	print (jogo)
 
 if __name__ == "__main__":
